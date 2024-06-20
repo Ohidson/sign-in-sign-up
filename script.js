e3aaa36c54f7ac7516d6df93739235a6770f7f55
Ohidson <ohidson@gmail.com>
2024-06-20 05:52:34 -0900

    new changes made

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ const signUp = document.querySelector("#signUp");
 const signIn = document.querySelector("#signIn");
 const heading = document.querySelector("#heading");
 const passWord = document.querySelector("#passWord");
+const name = document.getElementById("name");
 
 signIn.addEventListener('click', ()=>{
   heading.innerHTML = "Sign In";
-  passWord.style.display = "none"
+  name.style.display = "none"
 })
 
 signUp.addEventListener('click', ()=>{
   heading.innerHTML = "Sign Up";
-  passWord.style.display = "block"
+  name.style.display = "block"
 })
